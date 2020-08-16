756a22e88972ce7e5ea06dc9e9eb9bfa62d8d148
HXgrowns <804156017@qq.com>
2020-08-16 15:14:11 +0800

    [huxiao]: add calculator page

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,8 +7,15 @@ class Calculator extends Component {
       <section className="calculator">
         <h1 className="calc-title">在线计算器</h1>
         <section className="calc">
-          <section className="calc-layout">
-            计算器布局
+          <section className="calc-border">
+            <section className="calc-layout">
+              <input className="input" type="text"/>
+              <button className="op">+</button><button className="op">-</button><button className="op">*</button>
+              <button className="num">7</button><button className="num">8</button><button className="num">9</button>
+              <button className="num">4</button><button className="num">5</button><button className="num">6</button>
+              <button className="num">1</button><button className="num">2</button><button className="num">3</button>
+              <button className="num">0</button><button className="clear">Clear</button><button className="eq">=</button>
+            </section>
           </section>
           <a className="back-home" href="/">回到主页</a>
         </section>
